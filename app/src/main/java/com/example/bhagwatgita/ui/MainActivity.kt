a5084449330b3014ba822345757760ac307321f3
Rishi2419 <sahurishikesh1908@gmail.com>
2024-10-08 20:28:06 +0530

    "Verse of the day" issue fixed

--- app/src/main/java/com/example/bhagwatgita/ui/MainActivity.kt
+++ app/src/main/java/com/example/bhagwatgita/ui/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bhagwatgita.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.bhagwatgita.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
