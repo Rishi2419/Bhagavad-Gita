743617b67c7118d4753193f06e042a1797981527
Rishi2419 <sahurishikesh1908@gmail.com>
2026-06-11 20:08:53 +0530

    chk2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
 //                // your configuration here
 //                signingConfig = signingConfigs.getByName(name)
 //            }
-            isMinifyEnabled = false
+            isMinifyEnabled  = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
